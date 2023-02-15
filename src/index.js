6d6a231ee9001268335ba3d10d4e0ddae7bf9ae0
Prince Neres <prince84224960@gmail.com>
2023-02-15 17:38:53 -0300

    Create navbar component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navbar from './components/Navbar';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
   </React.StrictMode>
 )
