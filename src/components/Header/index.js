493e19af584db87358bbb3188cf5ab472392bdbd
Prince Neres <prince84224960@gmail.com>
2023-02-16 15:11:43 -0300

    Fix style of home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,9 +5,8 @@ export default function Header ({titulo, descricao, className = '', imagem}) {
     <header className={`${styles.header} ${className}`}>
       <div className={styles['header-texto']}>
         <h1>{titulo}</h1>
-        <h2>{descricao}</h2>
       </div>
-      <div>
+      <div className={styles['header-imagem']}>
         <img
           alt={titulo}
           src={imagem}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,40 @@
 import styles from './Home.module.scss';
 import Header from 'components/Header';
-import gamesBanner from 'assets/banner-games.jpg'
+import gamesBanner from 'assets/banner-games.jpg';
+import gamingDevices from 'assets/gaming-devices.jpg';
+import { useNavigate } from 'react-router-dom';
+
+const categories = [{
+  id: "Jogos",
+  name: "Jogos",
+  description: "Os melhores e mais populares jogos estão aqui!",
+  thumbnail: gamesBanner
+}]
 
 export default function Home () {
+
+  const navigate = useNavigate()
+
   return (
     <div>
       <Header
-        titulo="Jogos"
-        descrição="Adquira seus games aqui!"
-        imagem={gamesBanner}
+        titulo="Adquira seus produtos de gamer aqui!"
+        imagem={gamingDevices}
+        className={styles.header}
       />
+      <div className={styles.categories}>
+        <div className={styles['categories-title']}>
+          <h1>Categorias:</h1>
+        </div>
+        <div className={styles['categories-container']}>
+          {categories.map((category, index) => (
+            <div key={index} onClick={() => navigate(`/categoria/${category.id}`)}>
+              <img src={category.thumbnail} alt={category.name} />
+              <h1>{category.name}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 };
