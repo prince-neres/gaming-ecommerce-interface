e483ea928e505d86b28791afa9e87e97f31ca22a
Prince Neres <prince84224960@gmail.com>
2023-02-17 14:34:30 -0300

    Add page and route to cart

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,39 @@
+import styles from './Cart.module.scss';
+import Header from 'components/Header';
+import { useSelector } from 'react-redux';
+import Product from 'components/Product';
+
+export default function Cart () {
+
+  const cart = useSelector(state => {
+    const cartReduce = state.cart.reduce((products, productInCart) => {
+      const product = state.products.find(product => product.id === productInCart.id);
+      products.push({
+        ...product,
+        quantity: productInCart.quantity,
+      })
+      return products;
+    }, []);
+    return cartReduce;
+  });
+
+  console.log(cart)
+
+  return (
+    <div>
+      <Header
+        titulo='Carrinho de Compras'
+        descricao='Confira os produtos que você adicionou ao carrinho:'
+      />
+      <div className={styles.cart}>
+        {cart.map(product => <Product key={product.id} {...product}/>)}
+        <div className={styles.total}>
+          <strong>Resumo da compra</strong>
+          <span>
+            Subtotal: <strong>R$ {0.0.toFixed(2)}</strong>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import DefaultPage from 'components/DefaultPage';
 import Category from 'pages/Category';
 import Home from 'pages/Home';
+import Cart from 'pages/Cart';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<DefaultPage />}>
+        <Route path='/' element={<DefaultPage />} >
           <Route index element={<Home />} />
           <Route path='/categoria/:nameCategory' element={<Category />} />
+          <Route path='/carrinho' element={<Cart />} />
         </Route>
       </Routes>
     </BrowserRouter>
